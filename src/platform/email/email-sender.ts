e32a67e5546f09d03a5d74a1622e808ede38a247
pyxm1618 <146602164+pyxm1618@users.noreply.github.com>
2026-08-06 23:32:55 +0800

    feat: define transactional email boundary

---
+++ src/platform/email/email-sender.ts
@@ -0,0 +1,19 @@
+export type TransactionalEmailTemplate =
+  | "magic-link"
+  | "account-deletion"
+  | "security-notice";
+
+export type TransactionalEmail = {
+  readonly to: string;
+  readonly template: TransactionalEmailTemplate;
+  readonly subject: string;
+  readonly html: string;
+};
+
+export type EmailSendResult = {
+  readonly providerMessageId: string;
+};
+
+export interface EmailSender {
+  send(message: TransactionalEmail): Promise<EmailSendResult>;
+}
